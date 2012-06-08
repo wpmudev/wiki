bf71898c2ef538ab7f280ef0f394ac2c12993514
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-06-08 03:37:22 +0000

    New Notification plugin
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/wiki@6379 f53fe46e-8c02-459b-9fdd-93e75771b278

--- wiki.php
+++ wiki.php
@@ -32,12 +32,4 @@ if ( WIKI_DEMO_FOR_NON_SUPPORTER && function_exists('is_supporter') && !is_suppo
     include_once 'wiki-include.php';
 }
 
-if ( !function_exists( 'wdp_un_check' ) ) {
-    add_action( 'admin_notices', 'wdp_un_check', 5 );
-    add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-
-    function wdp_un_check() {
-        if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
-	    echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-    }
-}
+include_once 'lib/wpmudev-dashboard-notification/wpmudev-dash-notification.php';
