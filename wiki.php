c4cf62521033d8c67f4b276d53c6521052e77f3b
jonathan <jonathan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-01-04 23:39:37 +0000

    - updated version to 1.2.3.9
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/wiki@12357 f53fe46e-8c02-459b-9fdd-93e75771b278

--- wiki.php
+++ wiki.php
@@ -5,7 +5,7 @@
  Description: Add a wiki to your blog
  Author: S H Mohanjith (Incsub)
  WDP ID: 168
- Version: 1.2.3.8
+ Version: 1.2.3.9
  Author URI: http://premium.wpmudev.org
  Text Domain: incsub_wiki
 */
