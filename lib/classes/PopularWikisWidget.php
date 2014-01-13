a3e1e11759a5b9ebd0d6ecb726a3799abb999b32
jonathan <jonathan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-01-13 19:46:17 +0000

    - Code readability updates - Gettext domain updates
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/wiki@12494 f53fe46e-8c02-459b-9fdd-93e75771b278

--- lib/classes/PopularWikisWidget.php
+++ lib/classes/PopularWikisWidget.php
@@ -1,27 +1,21 @@
 <?php
 
 class PopularWikisWidget extends WP_Widget {
-    
-    /**
-     * @var		string	$translation_domain	Translation domain
-     */
-    var $translation_domain = 'wiki';
-    
     function __construct() {
-	$widget_ops = array( 'description' => __('Display Popular Wiki Pages', $this->translation_domain) );
-        $control_ops = array( 'title' => __('Popular Wikis', $this->translation_domain), 'hierarchical' => 'yes' );
-        
-	parent::WP_Widget( 'incsub_popular_wikis', __('Popular Wikis', $this->translation_domain), $widget_ops, $control_ops );
+    	global $wiki;
+			$widget_ops = array( 'description' => __('Display Popular Wiki Pages', 'wiki') );
+      $control_ops = array( 'title' => __('Popular Wikis', 'wiki'), 'hierarchical' => 'yes' );
+			parent::WP_Widget( 'incsub_popular_wikis', __('Popular Wikis', 'wiki'), $widget_ops, $control_ops );
     }
     
     function widget($args, $instance) {
-		global $wpdb, $current_site, $post, $wiki_tree;
+		global $wpdb, $current_site, $post, $wiki_tree, $wiki;
 		
 		extract($args);
 		
 		$options = $instance;
 		
-		$title = apply_filters('widget_title', empty($instance['title']) ? __('Popular Wikis', $this->translation_domain) : $instance['title'], $instance, $this->id_base);
+		$title = apply_filters('widget_title', empty($instance['title']) ? __('Popular Wikis', 'wiki') : $instance['title'], $instance, $this->id_base);
 		$hierarchical = $instance['hierarchical'];
 		
 		if ($hierarchical == 'yes') {
@@ -86,16 +80,17 @@ class PopularWikisWidget extends WP_Widget {
     }
     
     function update($new_instance, $old_instance) {
-	$instance = $old_instance;
-        $new_instance = wp_parse_args( (array) $new_instance, array( 'title' => __('Popular Wikis', $this->translation_domain), 'hierarchical' => 'yes') );
-        $instance['title'] = strip_tags($new_instance['title']);
-	$instance['hierarchical'] = $new_instance['hierarchical'];
-	
-        return $instance;
+    	global $wiki;
+			$instance = $old_instance;
+      $new_instance = wp_parse_args( (array) $new_instance, array( 'title' => __('Popular Wikis', 'wiki'), 'hierarchical' => 'yes') );
+      $instance['title'] = strip_tags($new_instance['title']);
+			$instance['hierarchical'] = $new_instance['hierarchical'];
+      return $instance;
     }
     
     function form($instance) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => __('Popular Wikis', $this->translation_domain), 'hierarchical' => 'yes'));
+    	global $wiki;
+			$instance = wp_parse_args( (array) $instance, array( 'title' => __('Popular Wikis', 'wiki'), 'hierarchical' => 'yes'));
 			$options = array('title' => strip_tags($instance['title']), 'hierarchical' => $instance['hierarchical']);
 		
 		if ($options['hierarchical'] == 'yes') {
@@ -105,15 +100,15 @@ class PopularWikisWidget extends WP_Widget {
 		}
 		?>
 		<div style="text-align:left">
-				<label for="<?php echo $this->get_field_id('title'); ?>" style="line-height:35px;display:block;"><?php _e('Title', $this->translation_domain); ?>:<br />
+				<label for="<?php echo $this->get_field_id('title'); ?>" style="line-height:35px;display:block;"><?php _e('Title', 'wiki'); ?>:<br />
 			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $options['title']; ?>" type="text" style="width:95%;" />
 				</label>
-				<label for="<?php echo $this->get_field_id('hierarchical'); ?>" style="line-height:35px;display:block;"><?php _e('Levels', $this->translation_domain); ?>:<br />
+				<label for="<?php echo $this->get_field_id('hierarchical'); ?>" style="line-height:35px;display:block;"><?php _e('Levels', 'wiki'); ?>:<br />
 						<select id="<?php echo $this->get_field_id('hierarchical'); ?>" name="<?php echo $this->get_field_name('hierarchical'); ?>" >
 					<?php for ($i=1; $i<5; $i++) { ?>
-					<option value="<?php echo $i; ?>" <?php if ($options['hierarchical'] == $i){ echo 'selected="selected"'; } ?> ><?php _e($i, $this->translation_domain); ?></option>
+					<option value="<?php echo $i; ?>" <?php if ($options['hierarchical'] == $i){ echo 'selected="selected"'; } ?> ><?php _e($i, 'wiki'); ?></option>
 					<?php } ?>
-					<option value="0" <?php if ($options['hierarchical'] == 0){ echo 'selected="selected"'; } ?> ><?php _e('Unlimited', $this->translation_domain); ?></option>
+					<option value="0" <?php if ($options['hierarchical'] == 0){ echo 'selected="selected"'; } ?> ><?php _e('Unlimited', 'wiki'); ?></option>
 						</select>
 					</label>
 			<input type="hidden" name="wiki-submit" id="wiki-submit" value="1" />
